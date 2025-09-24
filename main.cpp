8875f56bff1885144d5c97f38c402db11809077e
Rxv <stharishav7@gmail.com>
2025-09-24 21:37:33 +0545

    made some small changes overall

--- main.cpp
+++ main.cpp
@@ -1,44 +1,78 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-class Knight{
-public:
-    int health, stamina = 100;
-    int weapon_dur = 110;
+//------------------------------------------------------------------------
+//player class
+class player{
 
-private:
+public:
+   string name, faction;
+
+    player(string name, string faction){
+        this ->name = name;
+        this ->faction = faction;
+    }
+
+};
+//------------------------------------------------------------------------
+
+//------------------------------------------------------------------------
+//knight
+class KnightFaction{
+public:
+    int health, stamina, mana = 120;
+    int weapon_dur = 110;
     string weapon = "sword";
 
 };
+//------------------------------------------------------------------------
 
-Knight obj;
+//------------------------------------------------------------------------
+//Archer
+class ArcherFaction{
+public:
+    int health = 90, weapon_dur = 110;
+    int stamina = 120;
+    string weapon = "Bow";
+};
+//------------------------------------------------------------------------
 
-template<typename T>
-int weapon_dur_dmg(T &obj, int attacks){
+//------------------------------------------------------------------------
+//Mage
+class MageFaction{
 
-    int attacks_total = 55;
-    if(attacks < attacks_total){
-        for(int i = 0 ; i < (attacks_total-attacks); i++){
-            obj.weapon_dur =obj.weapon_dur - 2;
-        }
-    }
-    else if(attacks == 0){
-        cout <<"Your sword doesnot have any durability";
-    }
+public:
+    int health = 90, weapon_dur = 110;
+    int stamina = 90;
+    string weapon = " Magic Staff";
+};
+//-----------------------------------------------------------------------
 
-    return obj.weapon_dur;
-}
-
-
+//template<typename T>
+//int weapon_dur_dmg(T &obj, int attacks){
+//
+//    int attacks_total = 55;
+//    if(attacks < attacks_total){
+//        for(int i = 0 ; i < (attacks_total-attacks); i++){
+//            obj.weapon_dur =obj.weapon_dur - 2;
+//        }
+//    }
+//    else if(attacks == 0){
+//        cout <<"Your sword doesnot have any durability";
+//    }
+//
+//    return obj.weapon_dur;
+//}
 
 int main(){
-    int attacks;
-    cout << "How many attacks did you do? : ";
-    cin >> attacks;
 
-    cout << "Weapon durability: " << weapon_dur_dmg(obj, attacks) << endl;
+    string charname, whichfac;
+    cout << " what is your name and Which faction do you belong to? : ";
+    cin >> charname >> whichfac;
 
+    cout <<"Your name is "<< charname <<" and you belong to "<< whichfac
+         << " faction." << endl;
+    player p1(charname, whichfac);
 
 }
 
